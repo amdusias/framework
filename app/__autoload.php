467b6f1a64d0f0004cbde08c7f24bfb7272ecaa7
Nikolenko_Artem <artgomartgom@yandex.ru>
2023-02-17 09:34:08 +0500

    Переделано пространство имен под PSR-4

--- app/__autoload.php
+++ app/__autoload.php
@@ -3,7 +3,7 @@
 use Framework\Base\Autoload;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../framework/base/Autoload.php';
+require __DIR__ . '/../framework/Base/Autoload.php';
 
 spl_autoload_register(['\Framework\Base\Autoload', 'loader'], true, false);
 
